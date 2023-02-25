6d9ddc5fed98578bbc98c13736aaf178435ef5ac
Aditya <adityasingh220598@gmail.com>
2023-02-25 21:39:21 +0530

    Self hosted cors proxy server

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-// Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || "0.0.0.0";
-// Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8080;
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const request = require('request')
 
-var cors_proxy = require("cors-anywhere");
-cors_proxy
-  .createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: [],
-    removeHeaders: [],
-  })
-  .listen(port, host, function () {
-    console.log("Running CORS Anywhere on " + host + ":" + port);
+const app = express();
+
+// Load SSL/TLS certificate and key files
+const options = {
+  key: fs.readFileSync('/home/ubuntu/certificates/private.key'),
+  cert: fs.readFileSync('/home/ubuntu/certificates/certificatefinal.crt')
+};
+
+app.get('/suggestqueries', (req, res) => {
+    const url = `https://suggestqueries.google.com/complete/search?client=chrome&q=${req.query.q}`;
+  
+    // Set CORS headers to allow cross-origin requests
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+  
+    // Forward request to Google Suggest API endpoint
+    req.pipe(request(url)).pipe(res);
   });
+
+https.createServer(options, app).listen(443, () => {
+  console.log('HTTPS proxy server running on port 443');
+});
